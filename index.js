6d03e49ede59df31451b2125174c026934639805
ShashankShukla5 <shashank9450158183@gmail.com>
2025-05-04 19:56:14 +0530

    CORS

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cookieParser from "cookie-parser"
+import cors from "cors"
 
 import authRoutes from './src/routes/auth.route.js';
 import messageRoutes from './src/routes/message.route.js';
@@ -13,11 +14,17 @@ const app = express();
 
 const PORT = process.env.PORT;
 
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 app.use(express.json())
 app.use(cookieParser())
 
+
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
+
 
 app.listen(PORT, ()=> {
     console.log(`Server is running on port 5001: http://localhost:${PORT}`);
