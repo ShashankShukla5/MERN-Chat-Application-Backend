f66c459df7ea2bc38131abef206a0fd20b608637
ShashankShukla5 <shashank9450158183@gmail.com>
2025-05-04 18:20:01 +0530

    Backend First draft completed

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from "cookie-parser"
+
+import authRoutes from './src/routes/auth.route.js';
+import messageRoutes from './src/routes/message.route.js';
+
+import { connectDB } from './src/lib/db.js';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT;
+
+app.use(express.json())
+app.use(cookieParser())
+
+app.use("/api/auth", authRoutes)
+app.use("/api/message", messageRoutes)
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port 5001: http://localhost:${PORT}`);
+    connectDB()
+})
